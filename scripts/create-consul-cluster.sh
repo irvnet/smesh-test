abd4a76012d1437bbb6fdd6d1a33f034010b5418
irvnet <irvnet@gmail.com>
2022-04-09 20:50:55 -0400

    fix filename

--- scripts/create-consul-cluster.sh
+++ scripts/create-consul-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'creating consul cluster... '
-kind create cluster --name consul --config=../kind-config/consul-config.yaml
+kind create cluster --name consul --config=../kind-config/kind-config.yaml
 curl -sL https://run.linkerd.io/emojivoto.yml | kubectl apply -f -
 docker stop $(docker ps -a -q -f name="consul-control-plane")
 
