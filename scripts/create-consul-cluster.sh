66252f3f2159315d1b34d891590b6d63f9cc1550
irvnet <irvnet@gmail.com>
2022-04-09 21:41:44 -0400

    call emissary-config

--- scripts/create-consul-cluster.sh
+++ scripts/create-consul-cluster.sh
@@ -3,6 +3,7 @@
 echo 'creating consul cluster... '
 kind create cluster --name consul --config=../kind-config/kind-config.yaml
 curl -sL https://run.linkerd.io/emojivoto.yml | kubectl apply -f -
+./emissary-config.sh
 docker stop $(docker ps -a -q -f name="consul-control-plane")
 
 
