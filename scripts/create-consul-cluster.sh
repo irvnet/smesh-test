a7a7b43b6bbf740dd8c5da995b9799fe89ddb81c
irvnet <irvnet@gmail.com>
2022-04-15 08:31:42 -0400

    fix:cleanup, rename, simplify

--- scripts/create-consul-cluster.sh
+++ scripts/create-consul-cluster.sh
@@ -2,8 +2,8 @@
 
 echo 'creating consul cluster... '
 kind create cluster --name consul --config=../kind-config/kind-config.yaml
+kind create cluster --name consul --image kindest/node:v1.19.16 --config=../config/kind-cluster-config.yaml
 curl -sL https://run.linkerd.io/emojivoto.yml | kubectl apply -f -
-./emissary-config.sh
 docker stop $(docker ps -a -q -f name="consul-control-plane")
 
 
