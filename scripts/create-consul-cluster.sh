7d91b02e9a6df59e70b85bc96774cf1bbab5e8a2
irvnet <irvnet@gmail.com>
2022-04-15 08:54:33 -0400

    cleanup: eliminate unused code

--- scripts/create-consul-cluster.sh
+++ scripts/create-consul-cluster.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo 'creating consul cluster... '
-kind create cluster --name consul --config=../kind-config/kind-config.yaml
 kind create cluster --name consul --image kindest/node:v1.19.16 --config=../config/kind-cluster-config.yaml
-curl -sL https://run.linkerd.io/emojivoto.yml | kubectl apply -f -
-docker stop $(docker ps -a -q -f name="consul-control-plane")
+#docker stop $(docker ps -a -q -f name="consul-control-plane")
 
 
