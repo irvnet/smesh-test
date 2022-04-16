c8e379a41d999ed5ae3e75a9bc71329ef684223e
irvnet <irvnet@gmail.com>
2022-04-15 23:03:37 -0400

    add: gen tls certs

---
+++ scripts/secure-ingress-tls.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## check that the service is running
+curl -s -o /dev/null -w "Upstream Response Code: %{http_code}\n" http://localhost/
+
+## generate self-signed certificates for TLS config
+mkdir -p ~/work
+openssl rand -out ~/work/.rnd -writerand ~/work/.rnd
+
+## generate private key
+openssl req -x509 -newkey rsa:4096 -keyout ~/work/key.pem -out ~/work/cert.pem -subj '/CN=ambassador-cert' -nodes
+
+## add certs to a k8s secret
+kubectl create secret -n ambassador tls tls-cert --cert=~/work/cert.pem --key=~/work/key.pem
+
+
+
