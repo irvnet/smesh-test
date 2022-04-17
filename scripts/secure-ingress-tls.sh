dc834f2fb612477cc1919af49a04b48309729e8c
irvnet <irvnet@gmail.com>
2022-04-17 11:56:09 -0400

    fix: swap ~ for ${HOME}

--- scripts/secure-ingress-tls.sh
+++ scripts/secure-ingress-tls.sh
@@ -5,10 +5,10 @@ curl -s -o /dev/null -w "Upstream Response Code: %{http_code}\n" http://localhos
 
 ## generate self-signed certificates for TLS config
 mkdir -p ~/work
-openssl rand -out ~/work/.rnd -writerand ~/work/.rnd
+openssl rand -out ${HOME}/work/.rnd -writerand ${HOME}/work/.rnd
 
 ## generate private key
-openssl req -x509 -newkey rsa:4096 -keyout ~/work/key.pem -out ~/work/cert.pem -subj '/CN=ambassador-cert' -nodes
+openssl req -x509 -newkey rsa:4096 -keyout ${HOME}/work/key.pem -out ${HOME}/work/cert.pem -subj '/CN=ambassador-cert' -nodes
 
 ## add certs to a k8s secret
 kubectl create secret -n ambassador tls tls-cert --cert=${HOME}/work/cert.pem --key=${HOME}/work/key.pem
