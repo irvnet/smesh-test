f50f4ebdf6222a0ddabe127e874c6fb5a5aeb6f5
irvnet <irvnet@gmail.com>
2022-04-18 08:10:15 -0400

    fix: cleanup unused code

--- scripts/1-create-consul-cluster.sh
+++ scripts/1-create-consul-cluster.sh
@@ -2,6 +2,5 @@
 
 echo 'creating consul cluster... '
 kind create cluster --name consul --image kindest/node:v1.19.16 --config=../config/kind-cluster-config.yaml
-#docker stop $(docker ps -a -q -f name="consul-control-plane")
 
 
