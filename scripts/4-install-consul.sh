57b92ec76e4fe8cabd02d4250d9b3e56c8c03463
irvnet <irvnet@gmail.com>
2022-04-17 20:17:02 -0400

    add: consul install script

---
+++ scripts/4-install-consul.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+
+add hashicorp https://helm.releases.hashicorp.com
+
+kubectl create ns consul
+
+helm upgrade --install -n consul consul hashicorp/consul --wait -f ${HOME}/smesh-test/config/consul-global-settings.yaml
+
